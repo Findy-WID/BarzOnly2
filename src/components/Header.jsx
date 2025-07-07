1f7e7eaffadc659db22a5d53e4b8f03ca13eed64
Findy <favouritefindy@gmail.com>
2025-07-07 04:28:21 +0100

    improved the hero section and added a custom favicon icon

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,14 +4,18 @@ import hero1 from "../assets/hero1.jpg";
 import hero2 from "../assets/hero2.jpg";
 import hero3 from "../assets/hero3.jpg";
 
-const heroImages = [hero1, hero2, hero3];
+const heroData = [
+  {image: hero1, text: "Discover New Vibes🎧", para: "Stream your favorite tunes on repeat."},
+  {image: hero2, text: "Feel Da Heat🔥", para: "Dance to the rhythm non-stop"},
+  {image: hero3, text: "Flow with It🎵", para: "Every sound is worth it"}
+];
 
 export default function Header() {
-  const [activeHeroImg, setActiveHeroImg] = useState(0);
+  const [activeHeroData, setActiveHeroData] = useState(0);
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setActiveHeroImg((prev) => (prev + 1) % heroImages.length);
+      setActiveHeroData((prev) => (prev + 1) % heroData.length);
     }, 5000);
     return () => clearInterval(interval);
   }, []);
@@ -19,16 +23,16 @@ export default function Header() {
   return (
     <div className="w-full h-[200px] sm:h-[400px] mt-8 flex items-center justify-start gap-2 sm:gap-4 lg:gap-10 bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 rounded-xl overflow-hidden">
       <img
-        src={heroImages[activeHeroImg]}
+        src={heroData[activeHeroData].image}
         alt="hero"
         className="w-[120px] sm:w-[300px] lg:w-[400px] h-full object-cover rounded-l-lg"
       />
       <div className="flex flex-col">
         <h1 className="text-2xl sm:text-4xl lg:text-8xl font-extrabold leading-tight font-montserrat">
-          Discover <br /> New Vibes 🎧
+          {heroData[activeHeroData].text}
         </h1>
         <p className="text-sm sm:text-base lg:text-2xl text-gray-600 mt-1 sm:mt-2 lg:mt-4 font-montserrat">
-          Stream your favorite tunes on repeat.
+          {heroData[activeHeroData].para}
         </p>
       </div>
     </div>
